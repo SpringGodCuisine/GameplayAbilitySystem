612c8676ad8f7b8d5688068b7ea114a9d1c802aa
SpringGodCuisine <77707704+SpringGodCuisine@users.noreply.github.com>
2024-07-24 16:15:33 +0800

    增加DebugMode，增加Character相关类

---
+++ Source/GAS_RPG/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
