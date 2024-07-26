b2e9f1bef84c3ebec55ae36a91fe1c9534201273
SpringGodCuisine <77707704+SpringGodCuisine@users.noreply.github.com>
2024-07-26 14:00:27 +0800

    绑定增强移动

--- Source/GAS_RPG/Private/Player/AuraPlayerController.cpp
+++ Source/GAS_RPG/Private/Player/AuraPlayerController.cpp
@@ -1,9 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Player/AuraPlayerController.h"
 
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -38,3 +38,36 @@ void AAuraPlayerController::BeginPlay()
 	SetInputMode(InputModeData);
 }
 
+void AAuraPlayerController::SetupInputComponent()
+{
+	// 调用父类的 SetupInputComponent 方法
+	Super::SetupInputComponent();
+
+	// 将 InputComponent 强制转换为 UEnhancedInputComponent 类型
+	UEnhancedInputComponent * EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	// 绑定移动操作到 Move 函数，在触发事件发生时调用
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	// 从输入值中获取二维向量
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+
+	// 获取控制器的旋转角度
+	const FRotator Rotation = GetControlRotation();
+	// 仅保留 Yaw（偏航）角度，忽略 Pitch（俯仰）和 Roll（滚转）角度
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	// 获取沿前方向和右方向的单位向量
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	// 如果有控制的 Pawn，则根据输入值沿前方向和右方向添加移动输入
+	if(APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
