2ca7e18783e879c969688a1174faa0247ab5157c
SpringGodCuisine <77707704+SpringGodCuisine@users.noreply.github.com>
2024-07-26 21:54:44 +0800

    GameMode

--- Source/GAS_RPG/Private/Character/AuraCharacter.cpp
+++ Source/GAS_RPG/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,27 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	// 设置角色的移动方向与其朝向一致
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+    
+	// 设置角色旋转速率，只有 Yaw 轴（水平面上的旋转）为 400 度/秒
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+    
+	// 启用角色平面运动约束
+	GetCharacterMovement()->bConstrainToPlane = true;
+    
+	// 在游戏开始时将角色对齐到平面
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// 禁用控制器控制角色的俯仰角度（Pitch）
+	bUseControllerRotationPitch = false;
+	// 禁用控制器控制角色的滚转角度（Roll）
+	bUseControllerRotationRoll = false;
+	// 禁用控制器控制角色的偏航角度（Yaw）
+	bUseControllerRotationYaw = false;
+}
+
