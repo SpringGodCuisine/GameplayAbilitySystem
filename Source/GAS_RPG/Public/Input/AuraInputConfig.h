eb3263e1049c251410e5240dc4cab14896502d28
HARBORPC\83764 <837647312@qq.com>
2025-07-22 14:12:55 +0800

    增加输入映射

---
+++ Source/GAS_RPG/Public/Input/AuraInputConfig.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTags.h"
+#include "AuraInputConfig.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FAuraInputAction
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	const class UInputAction* InputAction = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag InputTag = FGameplayTag();
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API UAuraInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public: 
+
+	const UInputAction* FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound = false);
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FAuraInputAction> AbilityInputActions;
+};
