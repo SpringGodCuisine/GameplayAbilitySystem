47a1d13c4b673dd497d6979412a75debb38a6a8d
HARBORPC\83764 <837647312@qq.com>
2025-07-28 11:40:33 +0800

    通过事件生成火球

--- Source/GAS_RPG/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/GAS_RPG/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -12,7 +12,12 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+
+}
+
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
@@ -34,7 +39,4 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 		
 		Projectile->FinishSpawning(SpawnTransform);
 	}
-
-
-	
 }
