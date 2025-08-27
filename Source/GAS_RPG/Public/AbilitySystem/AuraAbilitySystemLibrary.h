d72cbfc96adc8426abb7514916fe9a7a005c97d7
HARBORPC\83764 <837647312@qq.com>
2025-08-27 14:09:28 +0800

    近战优化

--- Source/GAS_RPG/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/GAS_RPG/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -50,4 +50,7 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayMechanics")
 	static void GetLivePlayersWithinRadius(UPARAM(ref) const UObject* WorldContextObject, TArray<AActor*>& OutOverlappingActors, UPARAM(ref)  TArray<AActor*>& ActorsToIgnore, float Radius, const FVector& SphereOrigin);
+	
+	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|GameplayMechanics")
+	static bool IsNotFriend(AActor* FirstActor, AActor* SecondActor);
 };
