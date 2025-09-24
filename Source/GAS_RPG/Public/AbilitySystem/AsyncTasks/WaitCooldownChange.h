a15856d9121d8839f39ec6f6d311238556aff16b
HARBORPC\83764 <837647312@qq.com>
2025-09-24 19:05:19 +0800

    能力信息中的冷却标签

--- Source/GAS_RPG/Public/AbilitySystem/AsyncTasks/WaitCooldownChange.h
+++ Source/GAS_RPG/Public/AbilitySystem/AsyncTasks/WaitCooldownChange.h
@@ -15,7 +15,11 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCooldownChangedSignature, float, Ti
 /**
  * 
  */
-UCLASS()
+
+//ExposedAsyncProxy告诉 Unreal 引擎：
+//这个类（比如 UWaitCooldownChange、UAsyncTaskDownloadImage 之类的 UBlueprintAsyncActionBase 子类）是一个 异步代理类（Async Proxy）。
+//在蓝图里暴露时，用 "AsyncTask" 这个名字作为代理对象输出。
+UCLASS(BlueprintType, meta=(ExposedAsyncProxy = "AsyncTask"))
 class GAS_RPG_API UWaitCooldownChange : public UBlueprintAsyncActionBase
 {
 	GENERATED_BODY()
