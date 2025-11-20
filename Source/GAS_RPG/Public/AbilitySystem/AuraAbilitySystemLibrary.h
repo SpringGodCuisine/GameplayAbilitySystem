b01a9115c202ffc6a14c6507f6d99fea6203752f
SpringGodCuisine <77707704+SpringGodCuisine@users.noreply.github.com>
2025-11-20 21:44:05 +0800

    附加目标

--- Source/GAS_RPG/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/GAS_RPG/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -107,6 +107,9 @@ public:
 	
 	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayMechanics")
 	static void GetLivePlayersWithinRadius(UPARAM(ref) const UObject* WorldContextObject, TArray<AActor*>& OutOverlappingActors, UPARAM(ref)  TArray<AActor*>& ActorsToIgnore, float Radius, const FVector& SphereOrigin);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayMechanics")
+	static void GetClosestTargets(int32 MaxTargets, const TArray<AActor*>& Actors, TArray<AActor*>& OutClosestTargets, const FVector& Origin);
 	
 	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|GameplayMechanics")
 	static bool IsNotFriend(AActor* FirstActor, AActor* SecondActor);
